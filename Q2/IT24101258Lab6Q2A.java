0683927fd4b1db3299ed6e7987c4a94c1c79f2e7
It24101258 <it24101258@my.sliit.lk>
2024-09-09 22:57:53 +0530

    Add files via upload

---
+++ Q2/IT24101258Lab6Q2A.java
@@ -0,0 +1,10 @@
+public class IT24101258Lab6Q2A {
+   public static void main(String[] args) {
+       int i = 1;
+        
+       while (i <= 10) {
+           System.out.print(i + " ");
+           i++;
+        }
+    }
+}
