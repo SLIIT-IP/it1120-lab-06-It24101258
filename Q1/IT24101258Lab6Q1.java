eb20d47061643f902c37e03ca472b2ca1c6af2d7
It24101258 <it24101258@my.sliit.lk>
2024-09-09 22:57:36 +0530

    Add files via upload

---
+++ Q1/IT24101258Lab6Q1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IT24101258Lab6Q1 {
+   public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+
+       System.out.print("Enter a number: ");
+       double number = input.nextDouble();
+
+       double square = number * number;
+       double squareRoot = Math.sqrt(number);
+
+       System.out.println("The square of " + number + " is: " + square);
+       System.out.println("The square root of " + number + " is: " + squareRoot);
+    }
+}
